75579ad8f55236d878a9314053c2dda7d1a4f890
retlet <regist-github.com@retlet.net>
2010-03-04 02:49:39 +0900

    コメントアウト削除

--- extractor.pixivPhoto.js
+++ extractor.pixivPhoto.js
@@ -1,13 +1,13 @@
 (function() {
 Tombloo.Service.extractors.register({
-	name : 'Photo - pixiv',
-	ICON : 'http://www.pixiv.net/favicon.ico',
-    URL  : 'http://www.pixiv.net/',
-	check : function(ctx){
+  name : 'Photo - pixiv',
+  ICON : 'http://www.pixiv.net/favicon.ico',
+  URL  : 'http://www.pixiv.net/',
+  check : function(ctx){
         return ctx.onImage && 
             ctx.target.src.match(/^http:\/\/img\d+.pixiv.net\/img\//);
-	},
-	extract : function(ctx){
+  },
+  extract : function(ctx){
         if($x('//a[contains(@href, "mode=manga")]')) {
             ctx.target = {
                 src : ctx.target.src.replace(/_(?:m|s|100)\.(png|gif|jpe?g)$/, '_p0.$1'),
@@ -17,18 +17,7 @@ Tombloo.Service.extractors.register({
                 src : ctx.target.src.replace(/_(?:m|s|100)\.(png|gif|jpe?g)$/, '.$1'),
             };
         }
-		return Tombloo.Service.extractors['Photo - Upload from Cache'].extract(ctx);
-        
-        //var url;
-        //if (ctx.href.match(/^http:\/\/www\.pixiv\.net\/member_illust.php\?/.*mode=medium)) {
-        //    url = ctx.href;
-        //} else {
-        //    url = ctx.link.href;
-        //}
-		//return {
-        //    itemUrl   : ctx.target.src.replace(/_(?:m|s|100)\.(jpg|png|gif)$/, '.$1'),
-			//authorUrl : url,
-        //}
-	},
+        return Tombloo.Service.extractors['Photo - Upload from Cache'].extract(ctx);
+  },
 }, 'Photo', false);
 })();
