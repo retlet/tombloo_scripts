a65891c935b8943abcba6c8b93069796dcc20ffb
retlet <regist-github.com@retlet.net>
2010-03-09 14:49:26 +0900

    サイト追加

--- extractor.ProtectedSites.js
+++ extractor.ProtectedSites.js
@@ -1,3 +1,5 @@
 Tombloo.Service.extractors['Photo'].PROTECTED_SITES.push(
-    '//livedoor.blogimg.jp/minkch-two/'
+    '//livedoor.blogimg.jp/wakusoku/',
+    '//livedoor.blogimg.jp/minkch-two/',
+    '//livedoor.blogimg.jp/hamusoku/'
 );
