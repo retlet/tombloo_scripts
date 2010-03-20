c1a7de010f650916731fc04ec542bba355fe64c0
retlet <regist-github.com@retlet.net>
2010-03-20 15:35:48 +0900

    PROTECTED_SITES 変更と http://tumblr.g.hatena.ne.jp/retlet/20100320/1269065983 反映

--- extractor.ProtectedSites.js
+++ extractor.ProtectedSites.js
@@ -1,5 +1,3 @@
 Tombloo.Service.extractors['Photo'].PROTECTED_SITES.push(
-    '//livedoor.blogimg.jp/wakusoku/',
-    '//livedoor.blogimg.jp/minkch-two/',
-    '//livedoor.blogimg.jp/hamusoku/'
+    '//livedoor.blogimg.jp/'
 );
